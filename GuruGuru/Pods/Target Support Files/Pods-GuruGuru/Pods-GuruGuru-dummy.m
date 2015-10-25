9e2cfc9c7766f793fd8488ee161900ee367dcc42
螻ｱ譛ｬ縲\諱ｭ螟ｧ <hogazurou_0225@yahoo.co.jp>
2015-10-25 18:50:26 +0900

    pod　含めて試しにフルコミット

---
+++ GuruGuru/Pods/Target Support Files/Pods-GuruGuru/Pods-GuruGuru-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GuruGuru : NSObject
+@end
+@implementation PodsDummy_Pods_GuruGuru
+@end
---
+++ GuruGuru/Pods/Target Support Files/Pods-GuruGuru/Pods-GuruGuru-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GuruGuruVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GuruGuruVersionString[];
+
